25970cfc774465ba1cbd05e2526edd7434ec16aa
RAMIRO SUED <47958668@ort>
2024-08-05 11:33:35 -0300

    avances

--- app/Medico/[medicoId]/devolucion/page.js
+++ app/Medico/[medicoId]/devolucion/page.js
@@ -1,40 +1,48 @@
 "use client";
-import Link from "next/link";
 import { useParams } from "next/navigation";
-import { useState, useEffect } from "react";
+import { useState,useEffect } from "react";
 
-
-export default function Caso() {
-  const [medico, setMedico] = useState([]); // Inicializa con null o un objeto vacío
-  const params = useParams();
-
-  useEffect(() => {
-    async function fetchCaso() {
-      try {
-        const response = await fetch(`http://localhost:5000/medico/4/devolucion`); // Asegúrate de usar params.casoId si es necesario
-        if (!response.ok) {
-          throw new Error("Error al obtener los datos");
+export default function Devolucion(){
+    const params = useParams();
+    const [devoluciones, setDevoluciones] = useState([]);
+    console.log(params);
+    useEffect(() => {
+        async function fetchDevoluciones() {
+        try {
+            const response = await fetch(`http://localhost:5000/casos/${params.medicoId}/devolucion`);
+            if (!response.ok) {
+            throw new Error("Error al obtener los datos");
+            }
+            const data = await response.json();
+            setDevoluciones(data);
+        } catch (error) {
+            console.error("Error al obtener los casos:", error);
         }
-        const data = await response.json();
-        console.log(data);
-
-        setMedico(data);
-      } catch (error) {
-        console.error("Error al obtener el caso:", error);
-      }
-    }
-    fetchCaso();
-  }, []); 
-
-  return (
-    <main>
-      <div>            
-        <>          
-            
-        <h1>sss</h1>
-        </>            
-      </div>
-    </main>
+        }
+        fetchDevoluciones();
+    }, []);
     
-  );
+return(
+    <>
+    <h1>Devoluciones anteriores</h1>
+    <ul>
+    {devoluciones.map((devolucion)=>(
+    <li> {devolucion.Descripcion} / {devolucion.Fecha}</li>
+
+    )
+    )}
+    </ul>
+    <form id="devolucionForm" >
+                <label >Descripción:</label>
+                <input
+                    type="text"
+                    id="descripcion"
+                    name="descripcion"
+                    required
+                />
+                
+                <input type="submit" value="Agregar Devolución" />
+    </form>
+    </>
+)
 }
